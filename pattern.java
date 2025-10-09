b2bec50b426d726b5249e1608e95fa96dc2e53fe
ruthvika184 <ruthvika.1510@gmail.com>
2025-10-09 16:03:57 +0530

    Solved Q43: Printing Star Pattern

---
+++ pattern.java
@@ -0,0 +1,22 @@
+/*  
+if n=4
+* * * * 
+* * * * 
+* * * * 
+* * * * 
+*/
+
+
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++ ){
+            for(int j=1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
