3ef31a2e53a88a855ca546cfacf5a99ee815fa2a
ruthvika184 <ruthvika.1510@gmail.com>
2025-10-09 15:10:06 +0530

    Solved Q42: Sum of the given n numbers

---
+++ SumOfNum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class main{
+    public static void main (String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int sum=0;
+    for ( int i=0;i<n;i++){
+        int num = sc.nextInt();
+        sum = sum+num;
+    }
+    System.out.println(sum);
+    }
+}
