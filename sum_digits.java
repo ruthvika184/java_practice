9174daa77260058b4a5106af0b837e209e8913a6
ruthvika184 <ruthvika.1510@gmail.com>
2025-10-10 20:22:48 +0530

    Solved: Sum of n digits

---
+++ sum_digits.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int sum=0;
+        while(num>0){
+            sum = sum+(num%10);
+            num = num/10;
+        }
+        System.out.print(sum);
+    }
+}
