ceed4b6d6472ab2bbf079488ffa21b80659a8dfa
ruthvika184 <ruthvika.1510@gmail.com>
2025-10-21 21:55:08 +0530

    Solved: printing double characters of the string

---
+++ doubleChar.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String str1 = "";
+
+        for(int i=0;i<str.length();i++){
+           char ch = str.charAt(i);
+            str1 = str1+(ch+""+ch);
+        }
+        System.out.print(str1);
+    }
+}
