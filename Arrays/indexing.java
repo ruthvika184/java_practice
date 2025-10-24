8256c4c71a63f49d229f38edb0cca1596fc2f039
ruthvika184 <ruthvika.1510@gmail.com>
2025-10-24 10:48:16 +0530

    Solved: array indexing

---
+++ Arrays/indexing.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class main{
+    public static void main(String[]args){
+        String arr[] = {"Red","Orange","Yellow","Pink","Green","Blue","Purple","Black and White"};
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(arr[n]);
+    }
+}
