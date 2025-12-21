9a74b5f46735a99ee5b449f871102be64587fbd2
ruthvika184 <ruthvika.1510@gmail.com>
2025-12-21 19:09:45 +0530

    Solved: conditional statements example

---
+++ Methods/fizzbuzz.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Main {
+    static void fizzbuzz(int num) {
+        if(num % 3 ==0&& num%5==0){
+            System.out.println("FizzBuzz");
+        }else if(num%5==0){
+            System.out.println("Buzz");
+        }else if(num%3==0 ){
+            System.out.println("Fizz");
+        }else{
+            System.out.println(num);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int temp = sc.nextInt();
+        fizzbuzz(temp);
+        sc.close();
+    }
+}
